248ca9154163abe39dee9e76c20cbf35f9616d63
barry peddycord <ishara@isharacomix.org>
2018-07-27 19:10:04 +0000

    update year in test file

--- test/units/modules/network/cumulus/test_nclu.py
+++ test/units/modules/network/cumulus/test_nclu.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# (c) 2016, Cumulus Networks <ce-ceng@cumulusnetworks.com>
+# (c) 2016-2018, Cumulus Networks <ce-ceng@cumulusnetworks.com>
 #
 # This file is part of Ansible
 # Ansible is free software: you can redistribute it and/or modify
